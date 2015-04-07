fbd428033f5a1283f5be08507f11ff2263f1ba93
Stefan Lücke <glaxx@glaxx.net>
2015-04-07 20:02:10 +0200

    Implemented a SMTP client to send notifications

--- backend/mailnotify.go
+++ backend/mailnotify.go
@@ -20,22 +20,29 @@
 package backend
 
 import (
-	//	log "github.com/Sirupsen/logrus"
+	log "github.com/Sirupsen/logrus"
 	"gopkg.in/mgo.v2"
 	//	"gopkg.in/mgo.v2/bson"
-	//	"net/smtp"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"github.com/fatih/structs"
+	"net/smtp"
+	"strconv"
 	"sync"
-	//	"time"
+	"time"
 )
 
 type Mailconfig struct {
 	Enabled       bool
 	StartTLS      bool
 	ServerAddress string
+	Port          uint16
 	Username      string
 	Password      string
 	EMailAddress  string
-	Admins        []string
+	Admin         string
+	MaxAttempts   uint
 }
 
 func (m *Mailconfig) Verify() error {
@@ -52,32 +59,159 @@ type MailNotificationService struct {
 }
 
 type mail struct {
-	rcpt string
-	text string
+	header      header
+	status      uint
+	rcpt        string
+	body        string
+	nextAttempt time.Time
 }
 
-func NewMailNotificationService(user, deferred *mgo.Collection, mailcfg *Mailconfig) *MailNotificationService {
+type header struct {
+	From        string
+	Date        time.Time
+	Subject     string
+	To          string
+	ContentType string `mailheader:"Content-Type"`
+	ReturnPath  string `mailheader:"Return-Path"`
+}
+
+func (h *header) toByte() []byte {
+	var res string
+	for _, f := range structs.Fields(h) {
+		switch f.Value().(type) {
+		case string:
+			if t := f.Tag("mailheader"); t != "" {
+				res = res + fmt.Sprintf("%v: %v \n", t, f.Value())
+			} else {
+				res = res + fmt.Sprintf("%v: %v \n", f.Name(), f.Value())
+			}
+			break
+		case time.Time:
+			res = res + fmt.Sprintf("%v: %v\n", f.Name(), f.Value().(time.Time).Format(time.RFC1123Z))
+			break
+		}
+	}
+	res = res + "\n"
+	return []byte(res)
+}
+
+const (
+	mailStatusNew = iota
+	mailStatusPermanentFailure
+	mailStatusAttemptOffset
+)
+
+func NewMailNotificationService(user /*, deferred */ *mgo.Collection, mailcfg *Mailconfig) *MailNotificationService {
 	res := new(MailNotificationService)
 	res.user = user
-	res.deferred = deferred
+	//res.deferred = deferred
 	res.mc = mailcfg
+	res.status = make(chan int)
+	res.msg = make(chan mail)
+	res.wg.Add(1)
+	go res.processQueue()
 	return res
 }
 
 func (m *MailNotificationService) AddMailToQueue(rcpt, text string) {
+	ml := new(mail)
+	ml.status = mailStatusNew
+	ml.rcpt = rcpt
+	ml.body = text
+	ml.header.ContentType = "text/plain; charset=UTF-8"
+	ml.header.Date = time.Now()
+	ml.header.From = "lsmsd Notification Service <" + m.mc.EMailAddress + ">"
+	ml.header.ReturnPath = m.mc.Admin
+	ml.header.Subject = "Testnotify"
+	ml.header.To = rcpt
+	m.msg <- *ml
+}
+
+func (m *MailNotificationService) Quit() {
+	m.status <- 1
+	m.wg.Wait()
 }
 
 func (m *MailNotificationService) processQueue() {
+	defer m.wg.Done()
+	hit := false
+	for {
+		select {
+		case _ = <-m.status:
+			return
+		default:
+			select {
+			case ma := <-m.msg:
+				hit = true
+				err := m.sendMail(ma)
+				if err != nil {
+					//TODO: check for permanent failure
+					log.Warn(err)
+				}
+
+			default:
+			}
+		}
+		if !hit {
+			time.Sleep(200 * time.Millisecond)
+		} else {
+			hit = false
+		}
+	}
 }
 
-func (m *MailNotificationService) deferSend() {
+func (m *MailNotificationService) deferSend(ma mail) {
 }
 
 func (m *MailNotificationService) processDeferred() {
 }
 
-func (m *MailNotificationService) notifyAdmin() {
+func (m *MailNotificationService) notifyAdmin(ma mail) {
 }
 
-func (m *MailNotificationService) sendMail() {
+func (m *MailNotificationService) sendMail(ma mail) error {
+	auth := smtp.PlainAuth("", m.mc.Username, m.mc.Password, m.mc.ServerAddress)
+	c, err := smtp.Dial(m.mc.ServerAddress + ":" + strconv.FormatUint(uint64(m.mc.Port), 10))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	if m.mc.StartTLS {
+		if ok, _ := c.Extension("STARTTLS"); ok {
+			conf := new(tls.Config)
+			conf.ServerName = m.mc.ServerAddress
+			err = c.StartTLS(conf)
+			if err != nil {
+				return err
+			}
+		} else {
+			return errors.New("Server does not support StartTLS which is mandatory according to your settings")
+		}
+	}
+	err = c.Auth(auth)
+	if err != nil {
+		return err
+	}
+	err = c.Mail(m.mc.EMailAddress)
+	if err != nil {
+		return err
+	}
+	err = c.Rcpt(ma.rcpt)
+	if err != nil {
+		return err
+	}
+	data, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = data.Write(ma.header.toByte())
+	if err != nil {
+		return err
+	}
+	_, err = data.Write([]byte(ma.body))
+	if err != nil {
+		return err
+	}
+	return data.Close()
 }
