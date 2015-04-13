b1dee2b41771a46022eaf8d9fe58c97ea1831011
Stefan Lücke <glaxx@glaxx.net>
2015-04-13 12:37:19 +0200

    Started writing ginkgo test for /item

--- webservice/item_test.go
+++ webservice/item_test.go
@@ -17,127 +17,77 @@
  *    Authors: Stefan Luecke <glaxx@glaxx.net>
  */
 
-package webservice
+package webservice_test
 
 import (
-	//	"encoding/json"
 	"github.com/emicklei/go-restful"
 	db "github.com/openlab-aux/lsmsd/database"
-	//	dmp "github.com/sergi/go-diff/diffmatchpatch"
+	//	. "github.com/openlab-aux/lsmsd/webservice"
+	"bytes"
+	//	"encoding/json"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"gopkg.in/mgo.v2"
-	//	"math/rand"
 	"net/http"
 	"net/http/httptest"
-	"strconv"
-	"testing"
 )
 
-func newTestContainer(t testing.TB) (*mgo.Session, *restful.Container, *db.ItemDBProvider) {
-	s, err := mgo.Dial("localhost")
-	if err != nil {
-		t.Error(err.Error())
-	}
-	cont := restful.NewContainer()
+var _ = Describe("Item", func() {
+	var (
+		session *mgo.Session
+		cont    *restful.Container
+		itm     *db.ItemDBProvider
+		pol     *db.PolicyDBProvider
+		usr     *db.UserDBProvider
+		hw      *httptest.ResponseRecorder
+		req     *http.Request
+		body    []byte
+	)
 
-	itemp := db.NewItemDBProvider(s, "lsmsd_test")
-	polp := db.NewPolicyDBProvider(s, "lsmsd_test")
-	userp := db.NewUserDBProvider(s, itemp, polp, "lsmsd_test")
-	auth := NewBasicAuthService(userp)
-	iws := NewItemWebService(itemp, auth)
-	pws := NewPolicyService(polp, auth)
-	uws := NewUserService(userp, auth)
-	cont.Add(iws.S)
-	cont.Add(pws.S)
-	cont.Add(uws.S)
-	return s, cont, itemp
-}
+	BeforeEach(func() {
+		session, cont, itm, pol, usr = newTestContainer()
+		hw = httptest.NewRecorder()
+	})
 
-func flushDB(t testing.TB, s *mgo.Session, itm *db.ItemDBProvider) {
-	itm.Stop()
-	err := s.DB("lsmsd_test").DropDatabase()
-	if err != nil {
-		t.Error("failed to clean up: " + err.Error())
-	}
-	s.Close()
-}
+	AfterEach(func() {
+		flushDB(session, itm)
+	})
 
-func Test_GetItemByIdReturns400InvalidID(t *testing.T) {
-	s, cont, it := newTestContainer(t)
-	defer flushDB(t, s, it)
-	httpRequest, _ := http.NewRequest("GET", "/item/232323", nil)
-	httpRequest.Header.Set("Content-Type", restful.MIME_JSON)
-	httpWriter := httptest.NewRecorder()
+	Describe("Retrieve a item from the database", func() {
+		Context("With an invalid identifier", func() {
+			JustBeforeEach(func() {
+				rd := bytes.NewReader(body)
+				req, _ = http.NewRequest("GET", "/item/ſhịŧ", rd)
+				req.Header.Set("Content-Type", restful.MIME_JSON)
+			})
+			It("should be a bad request", func() {
+				cont.ServeHTTP(hw, req)
+				Expect(hw.Code).To(Equal(http.StatusBadRequest))
+			})
+		})
 
-	cont.ServeHTTP(httpWriter, httpRequest)
-	if httpWriter.Code != http.StatusNotFound {
-		t.Error("failed expected:" + strconv.Itoa(http.StatusNotFound) + "\n Got:" + strconv.Itoa(httpWriter.Code))
-	}
-}
+		Context("With valid identifier", func() {
+			JustBeforeEach(func() {
+				rd := bytes.NewReader(body)
+				req, _ = http.NewRequest("GET", "/item/1", rd)
+				req.Header.Set("Content-Type", restful.MIME_JSON)
+			})
+			Context("which does not exist", func() {
+				It("should return 404 not found", func() {
+					cont.ServeHTTP(hw, req)
+					Expect(hw.Code).To(Equal(http.StatusNotFound))
+				})
+			})
 
-func Test_GetItemByIdReturns500BadRequest(t *testing.T) {
-	s, cont, it := newTestContainer(t)
-	defer flushDB(t, s, it)
-
-	req, _ := http.NewRequest("GET", "/item/ſḥịŧ", nil)
-	req.Header.Set("Content-Type", restful.MIME_JSON)
-	hw := httptest.NewRecorder()
-
-	cont.ServeHTTP(hw, req)
-	if hw.Code != http.StatusBadRequest {
-		t.Error("failed. expected:" + strconv.Itoa(http.StatusBadRequest) + "\n Got:" + strconv.Itoa(hw.Code))
-	}
-}
-
-/*func TestGetItemById(t *testing.T) {
-	s, cont := newTestContainer(t)
-	defer flushDB(t, s)
-	itm := db.Item{EID: 23, Name: "testitem", Description: "testdesc", Contains: []uint64{2, 3}, Owner: "testuser", Maintainer: "test", Usage: "test", Discard: "disc"}
-	id, err := reateItem(&itm)
-	if err != nil {
-		t.Error("failed: " + err.Error())
-	}
-
-	req, _ := http.NewRequest("GET", "/item/"+strconv.FormatUint(id, 10), nil)
-	req.Header.Set("Content-Type", restful.MIME_JSON)
-	hw := httptest.NewRecorder()
-
-	cont.ServeHTTP(hw, req)
-	if hw.Code != http.StatusOK {
-		t.Error("failed. expected:" + strconv.Itoa(http.StatusOK) + "\n Got:" + strconv.Itoa(hw.Code))
-	}
-	body := hw.Body.Bytes()
-	jitm := new(Item)
-	err = json.Unmarshal(body, jitm)
-	if err != nil {
-		t.Error("failed: " + err.Error())
-	}
-	if itm.EID != jitm.EID {
-		t.Error("id injection")
-	}
-	if itm.Name != jitm.Name {
-		t.Error()
-	}
-	if itm.Description != jitm.Description {
-		t.Error()
-	}
-	if len(itm.Contains) != len(jitm.Contains) {
-		t.Error()
-	}
-	for i := 0; i != len(itm.Contains); i++ {
-		if itm.Contains[i] != jitm.Contains[i] {
-			t.Error()
-		}
-	}
-	if itm.Owner != jitm.Owner {
-		t.Error()
-	}
-	if itm.Maintainer != jitm.Maintainer {
-		t.Error()
-	}
-	if itm.Usage != jitm.Usage {
-		t.Error()
-	}
-	if itm.Discard != jitm.Discard {
-		t.Error()
-	}
-}*/
+			Context("which does exist", func() {
+				BeforeEach(func() {
+					populateItemDB(itm)
+				})
+				It("should return 200 ok", func() {
+					cont.ServeHTTP(hw, req)
+					Expect(hw.Code).To(Equal(http.StatusOK))
+				})
+			})
+		})
+	})
+})
